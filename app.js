580cda4a76bee8e9ef344847728ca4c2dcc14235
Tina Wang <tina@tina.cafe>
2022-02-24 00:33:20 +0100

    🛂 Use new tanabata auth method

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import got from 'got';
 dotenv.config();
 
 const contract_address = "0xc3f8a0f5841abff777d3eefa5047e8d413a1c9ab";
-const tanabata_api = got.extend({ prefixUrl: "https://tanabata.tina.cafe/pak/", headers: { secret: process.env.TANABATA_SECRET }, responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'], calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
+const tanabata_api = got.extend({ prefixUrl: "https://tanabata.tina.cafe/pak/", headers: { 'X-API-KEY': process.env.TANABATA_API_KEY }, responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'], calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
 const web3_api = got.extend({ prefixUrl: "https://node1.web3api.com/", responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'], calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
 const alchemy_api = got.extend({ prefixUrl: "https://eth-mainnet.alchemyapi.io/jsonrpc/ER1Uh6Lu38x2xWXc7IomSmYFO5twNigV", responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'] } });
 const os_api = got.extend({ prefixUrl: "https://api.opensea.io/api/v1/", responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
