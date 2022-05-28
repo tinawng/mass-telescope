0b9399d45a76954700ea2f42ee0c0386587b0514
Tina Wang <tina@tina.cafe>
2022-05-28 20:05:57 +0200

    ⚡Change apis handling

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import got from 'got';
 dotenv.config();
 
 const contract_address = "0xc3f8a0f5841abff777d3eefa5047e8d413a1c9ab";
-const tanabata_api = got.extend({ prefixUrl: "https://tanabata.tina.cafe/merge/", headers: { 'X-API-KEY': process.env.TANABATA_API_KEY }, responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'], calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
+const tanabata_api = got.extend({ prefixUrl: "https://tanabata.tina.cafe/merge/", headers: { 'X-API-KEY': process.env.TANABATA_API_KEY }, responseType: 'json', resolveBodyOnly: true });
 const web3_api = got.extend({ prefixUrl: "https://node1.web3api.com/", responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'], calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
 const alchemy_api = got.extend({ prefixUrl: "https://eth-mainnet.alchemyapi.io/jsonrpc/ER1Uh6Lu38x2xWXc7IomSmYFO5twNigV", responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, methods: ['POST'] } });
 const os_api = got.extend({ prefixUrl: "https://api.opensea.io/api/v1/", responseType: 'json', resolveBodyOnly: true, retry: { limit: 10, calculateDelay: ({ attemptCount }) => attemptCount * 2000 } });
@@ -19,16 +19,16 @@ const known_merged = await tanabata_api('merged_tokens');
 
 console.time(`overall`);
 // ⚫️ Parsing all tokens: 130 * 223 = 28,990
-for (let chunk = 0; chunk < 130; chunk++) {
+for (let chunk = 0; chunk < 223; chunk++) {
     // ⚡️ Making url list for // request exec
     let urls = [];
-    for (let i = 1; i <= 223; i++) {
-        let hex = (223 * chunk + i).toString(16);
+    for (let i = 1; i <= 130; i++) {
+        let hex = (130 * chunk + i).toString(16);
         let b32 = '0xc87b56dd' + hex.padStart(64, '0');
         urls.push(
             new Promise(async (resolve, reject) => {
-                let api_resp = await web3_api.post('', { json: { "jsonrpc": "2.0", "id": (223 * chunk + i), "method": "eth_call", "params": [{ "from": "0x0000000000000000000000000000000000000000", "data": b32, "to": contract_address }, "latest"] }, headers: { referer: 'etherscan.io' } }).json();
-                
+                let api_resp = await web3_api.post('', { json: { "jsonrpc": "2.0", "id": (130 * chunk + i), "method": "eth_call", "params": [{ "from": "0x0000000000000000000000000000000000000000", "data": b32, "to": contract_address }, "latest"] }, headers: { referer: 'etherscan.io' } }).json();
+
                 let metadata_b64, merged_to, merged_on, sale_price;
                 if (api_resp.error) {
                     // 💫 This is a merged token
@@ -43,7 +43,7 @@ for (let chunk = 0; chunk < 130; chunk++) {
                         if (merged_to) {
                             [metadata_b64, sale_price, merged_on] = await askOpenSea(api_resp.id);
                             if (!sale_price)
-                                [merged_on, sale_price] = await askNiftyMarket(api_resp.id);
+                                sale_price = await askNiftyMarket(api_resp.id);
                         }
                         else {
                             // 🌱  merged_to is undefined, token has been merged before the re-mint
@@ -79,7 +79,7 @@ for (let chunk = 0; chunk < 130; chunk++) {
     let tokens = await Promise.all(urls);
     console.timeEnd(`tokens   ${chunk}`);
 
-    await tanabata_api.post('merges', { json: tokens });
+    await tanabata_api.put('merge_tokens', { json: tokens });
 }
 
 await tanabata_api.post('snap_history');
@@ -98,20 +98,21 @@ async function askOpenSea(id) {
     var metadata_b64 = token_metadata.split('json;base64,')[1];
     let merged_on, sale_price;
     if (last_sale) {
-        if (last_sale.payment_token.symbol === 'ASH')
+        if (last_sale.payment_token?.symbol === 'ASH')
             sale_price = (last_sale.payment_token.eth_price * 10) * last_sale.total_price / 10e17;
         else sale_price = last_sale.total_price / 10e17;
-        merged_on = last_sale.event_timestamp;
     }
+    let resp = await os_api(`events?only_opensea=false&token_id=${id}&asset_contract_address=${contract_address}&event_type=transfer`, { headers: { 'X-API-KEY': process.env.OPENSEA_API_KEY.split(',')[1] } }).json();
+    merged_on = resp?.asset_events[0]?.created_date;
+
     return [metadata_b64, sale_price, merged_on]
 }
 
 async function askNiftyMarket(id) {
     let resp = await nifty_market_api.post('', { json: { "contractAddress": contract_address, "current": 1, "size": 1, "tokenId": id } }).json();
 
-    if (resp?.data?.results[0]?.Type == "sale") return [resp.data.results[0].Timestamp, resp.data.results[0].SaleAmountInCents / 100 / eth_usd];
-    if (resp?.data?.results[0]?.Type == "nifty_transfer") return [resp.data.results[0].Timestamp, undefined];
-    return [undefined, undefined]
+    if (resp?.data?.results[0]?.Type == "sale") return resp.data.results[0].SaleAmountInCents / 100 / eth_usd;
+    return undefined
 }
 async function askNiftyMetadata(id) {
     let { niftyMetadata } = await nifty_metadata_api.get(`?contractAddress=${contract_address}&tokenId=${id}`).json();
